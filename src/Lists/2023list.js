e7516ec7c83b3032d19713d76a6c9d7e1605f7b5
Joaquin Villalba <68406893+jnvillalba@users.noreply.github.com>
2023-02-02 20:48:41 -0300

    cambio estreno antman
            titulo:"Ant-Man & The Wasp: Quantumania",estreno:"28/07/2023"

--- src/Lists/2023list.js
+++ src/Lists/2023list.js
@@ -4,7 +4,7 @@ const content2023List = [
         titulo:"Ant-Man & The Wasp: Quantumania",
         tipo:"Pelicula",
         poster:"https://m.media-amazon.com/images/M/MV5BNDgyNGM4NTYtN2M3MS00YTY2LTk0OWUtZmIzYjg3MmQ0OGM4XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
-        estreno:"17/02/2023"
+        estreno:"15/02/2023"
         
     },
 
