9adf5d40d7de11519a3a452a8f98f8293417879b
jnvillalba <joaquinvillalba8@gmail.com>
2023-05-18 18:41:47 -0300

    cambios en el estilo

--- src/Components/Studio/Studio.jsx
+++ src/Components/Studio/Studio.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function Studio({ poster, index, video }) {
   return (
     <div className="icon-cards__item d-flex align-items-center justify-content-center">
-      <span className="h1">
-        <img className="studio" src={poster} alt="Logo" onClick={index} />
-      </span>
+      <img className="studio" src={poster} alt="Logo" onClick={index} />
     </div>
   );
 }
